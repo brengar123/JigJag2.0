97df2b7c8984fcbbf48427fab6064cb30819401a
brengar123 <garvinwang12@hotmail.com>
2020-04-22 16:41:49 +1000

    created login page

--- app/src/main/java/com/example/jigjag20/MainActivity.java
+++ app/src/main/java/com/example/jigjag20/MainActivity.java
@@ -5,66 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "Psychometric testing";
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+public Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mRecyclerView = findViewById(R.id.RvList);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        GamesAdapter.RecyclerViewClickListener listener = new GamesAdapter.RecyclerViewClickListener(){
+        mButton = findViewById(R.id.login);
+        mButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view, int position){
-                launchGame(position);
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+                startActivity(intent);
             }
-        };
-        mAdapter = new GamesAdapter(Games.getGames(), listener);
-        mRecyclerView.setAdapter(mAdapter);
-
-    }
-    private void launchgameone(){
-        Intent intent = new Intent(this, GameOneFragment.class);
-        startActivity(intent);
-    }
-    private void launchgametwo(){
-        Intent intent = new Intent(this, GameTwoFragment.class);
-        startActivity(intent);
-    }
-    private void launchgamethree() {
-        Intent intent = new Intent(this, GameThreeFragment.class);
-        startActivity(intent);
-    }
-    private void launchgamefour() {
-        Intent intent = new Intent(this, GameFourFragment.class);
-        startActivity(intent);
-    }
-    private void launcheasteregg(){
-        Intent intent = new Intent(this, EasterEgg.class);
-        startActivity(intent);
+        });
     }
 
-    private void launchGame(int position) {
-        switch (position){
-            case 0: launchgameone(); break;
-            case 1: launchgametwo(); break;
-            case 2: launchgamethree(); break;
-            case 3: launchgamefour(); break;
-            case 4: launcheasteregg(); break;
-        }
-    }
+
 }
